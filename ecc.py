74396cc20f4566df5a8a2c647c3c0477c6338843
svivek1997 <47782502+svivek1997@users.noreply.github.com>
2023-03-31 00:42:17 -0500

    Create ecc.py
    initial commit

---
+++ ecc.py
@@ -0,0 +1,30 @@
+import collections
+import hashlib
+import random
+
+EllipticCurve = collections.namedtuple('EllipticCurve', 'name p a b g n h')
+
+curve = EllipticCurve(
+    'secp256k1',
+    # Field characteristic.
+    p=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f,
+    # Curve coefficients.
+    a=0,
+    b=7,
+    # Base point.
+    g=(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+       0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8),
+    # Subgroup order.
+    n=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141,
+    # Subgroup cofactor.
+    h=1,
+)
+
+# Keypair generation and ECDSA ################################################
+
+def make_keypair():
+    """Generates a random private-public key pair."""
+    private_key = random.randrange(1, curve.n)
+    public_key = scalar_mult(private_key, curve.g)
+
+    return private_key, public_key
